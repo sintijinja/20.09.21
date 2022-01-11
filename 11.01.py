22f34eb5e8d9ab32c6d3729e347e1836b564bca5
sincuks <91064799+sintijinja@users.noreply.github.com>
2022-01-11 13:10:41 +0000

    11.01

---
+++ 11.01.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+lapa = requests.get("https://vvsprogramm.github.io/2021_debug/")
+print(lapa)
+print(lapa.content)
+
+soup = BeautifulSoup(lapa.content, 'html.parser')
+print(soup.prettify())
+
+print([type(item) for item in list(soup.children)])
+
+html = list(soup.children)[2]
+print(list(html.children))
+
+body = list(html.children)[3]
+print(list(body.children))
+
+p = list(body.children)[1]
+print(p.get_text())
